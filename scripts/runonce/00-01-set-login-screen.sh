fd8e2f3625602612986a0f1321081c72eeb96c70
Burke Azbill <burkeazbill@users.noreply.github.com>
2023-05-08 11:42:53 -0400

    Update login-screen scripts

--- scripts/runonce/00-01-set-login-screen.sh
+++ scripts/runonce/00-01-set-login-screen.sh
@@ -8,6 +8,25 @@ echo
 echo
 
 echo "Updating Login Screen" > ~/.status.txt
+
+#
+# The custom-login-screen will be displayed at the console login
+# Additionally, if used on a VMware Horizon Desktop, this login screen will be presented
+# during login/SSO setup
+# During this build stage, we download and install the utility.
+# After build user initial login, the utility will be executed once to set the background
+#
+# See if our custom login screen is in our Pictures folder of the build user's Home directory:
+if [ -f "$HOME/Pictures/custom-login-screen.png" ]; then
+  login_background_source="/usr/share/backgrounds/custom-login-screen.png"
+  sudo cp "$HOME/Pictures/custom-login-screen.png" "$login_background_source"
+  sudo chmod 644 "$login_background_source"
+  # This 3rd party tool is no longer being used due to native commands available. See the 00-0-gnome-settings.sh for details
+  #wget -qO - https://github.com/PRATAP-KUMAR/ubuntu-gdm-set-background/archive/main.tar.gz | tar zx --strip-components=1 ubuntu-gdm-set-background-main/ubuntu-gdm-set-background
+  #sudo mv ubuntu-gdm-set-background /usr/local/bin
+fi
+
+
 # Try to do some GDM settings as the gdm user:
 # dbus-x11 is required for the commands to run as the gdm user
 sudo apt install -y dbus-x11
@@ -19,8 +38,8 @@ chmod 755 "$HOME"/.scripts/gset.sh
 cp "$HOME"/.scripts/gset.sh /tmp
 # Configure the Login Screen -- SPECIAL steps required - must be done as gdm user
 # NOTE: If you specify a background color then the background-picture-uri is ignored
-if [ -f "/usr/share/backgrounds/custom-login-screen.png" ]; then
-  sudo -u gdm /tmp/gset.sh /tmp/.Xauthority com.ubuntu.login-screen background-picture-uri '/usr/share/backgrounds/custom-login-screen.png'
+if [ -f "$login_background_source" ]; then
+  sudo -u gdm /tmp/gset.sh /tmp/.Xauthority com.ubuntu.login-screen background-picture-uri "$login_background_source"
   sudo -u gdm /tmp/gset.sh /tmp/.Xauthority com.ubuntu.login-screen background-size 'auto'
 else
   sudo -u gdm /tmp/gset.sh /tmp/.Xauthority com.ubuntu.login-screen background-color '#234f79'
@@ -40,6 +59,6 @@ fi
 # sudo -u gdm /tmp/gset.sh /tmp/.Xauthority org.gnome.login-screen fallback-logo ''
 # The following icon shows up BENEATH the username box on the login screen:
 # sudo -u gdm /tmp/gset.sh /tmp/.Xauthority org.gnome.login-screen logo '/usr/share/icons/my-logo.png'
-sudo -u gdm /tmp/gset.sh /tmp/.Xauthority org.gnome.login-screen banner-message-enable true
+sudo -u gdm /tmp/gset.sh /tmp/.Xauthority org.gnome.login-screen banner-message-enable false
 sudo -u gdm /tmp/gset.sh /tmp/.Xauthority org.gnome.login-screen banner-message-text 'Horizon Users: SSO will auto-login shortly.'
 sudo -u gdm /tmp/gset.sh /tmp/.Xauthority org.gnome.login-screen disable-user-list true
