3ec665dd2eb69d9af8206adfee07dcae8852e482
Burke Azbill <burkeazbill@users.noreply.github.com>
2023-05-16 15:35:25 -0400

    Update 05-install-OpenLens.sh
    Fix grep statement to get amd64.deb

--- scripts/runonce/05-install-OpenLens.sh
+++ scripts/runonce/05-install-OpenLens.sh
@@ -9,7 +9,7 @@ echo "Installing OpenLens" > ~/.status.txt
 # [^arm64] <- don't return the lines with arm64 before the .deb
 # Only return the line that ends with .deb"
 if [ ! -f /usr/bin/open-lens ]; then
-  curl -s https://api.github.com/repos/MuhammedKalkan/OpenLens/releases/latest | grep 'browser_download_url.*[^arm64]\.deb"$' | cut -d '"' -f 4 | wget -i -
+  curl -s https://api.github.com/repos/MuhammedKalkan/OpenLens/releases/latest | grep 'browser_download_url.*amd64.deb"$' | cut -d '"' -f 4 | wget -i -
   sudo dpkg -i OpenLens*.deb
   rm -f OpenLens*.deb
 fi
